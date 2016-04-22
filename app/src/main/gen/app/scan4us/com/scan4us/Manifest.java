e8aa85a38ab0093df516d9fbb38110ba087e3435
Juan Camilo Villa <juancamilov06@hotmail.com>
2016-04-22 00:03:29 -0500

    First commit

---
+++ app/src/main/gen/app/scan4us/com/scan4us/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package app.scan4us.com.scan4us;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
