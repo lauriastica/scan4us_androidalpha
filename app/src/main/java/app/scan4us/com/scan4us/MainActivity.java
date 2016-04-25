47ec87a1efa6ea8e671d0c01c592d6e2f23e7490
Juan Camilo Villa <juancamilov06@hotmail.com>
2016-04-24 19:24:05 -0500

    Layouts added, fonts missing, some colors missing, some layouts missing

--- app/src/main/java/app/scan4us/com/scan4us/MainActivity.java
+++ app/src/main/java/app/scan4us/com/scan4us/MainActivity.java
@@ -3,14 +3,11 @@ package app.scan4us.com.scan4us;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import app.scan4us.com.scan4us.activities.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,21 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-              startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
-            }
-        });
-
-  /*
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                        */
     }
 
     @Override
---
+++ app/src/main/java/app/scan4us/com/scan4us/activities/LandingActivity.java
@@ -0,0 +1,19 @@
+package app.scan4us.com.scan4us.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import app.scan4us.com.scan4us.R;
+
+/**
+ * Created by juanc on 23/04/2016.
+ */
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
